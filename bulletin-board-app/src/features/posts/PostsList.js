89f6626d70b3f1e6578462b1c58cf2f666e59fc2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 15:48:36 +0300

    Removed the article markup and all its contents to the renderedPosts arrow function

--- bulletin-board-app/src/features/posts/PostsList.js
+++ bulletin-board-app/src/features/posts/PostsList.js
@@ -18,15 +18,7 @@ const PostsList = () => {
     
 
     const renderedPosts = posts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0, 100)}</p>
-            <p className="postCredit">
-                <PostAuthor userId={post.userId} />
-                <TimeAgo timestamp={post.date} />
-            </p>
-            <ReactionButtons post={post}/>
-        </article>
+        
     ))
 
     return (
