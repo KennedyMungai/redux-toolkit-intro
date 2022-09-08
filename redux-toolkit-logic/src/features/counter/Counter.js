e58aa2f57f2aa8087ca2ae98295f88b2a4515b4b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 10:31:25 +0300

    Created the resetAll arrow function that sets the increment amount to zero and called the reset action via dispatch

--- redux-toolkit-logic/src/features/counter/Counter.js
+++ redux-toolkit-logic/src/features/counter/Counter.js
@@ -12,10 +12,13 @@ import {
 const Counter = () => {
     const count = useSelector((state) => state.counter.count);
     const dispatch=useDispatch();
-
     const [incrementAmount, setIncrementAmount] = useState(0);
-
     const addValue=Number(incrementAmount) || 0;
+
+    const resetAll = () => {
+        setIncrementAmount(0);
+        dispatch(reset());
+    };
 
     return (
         <section>
