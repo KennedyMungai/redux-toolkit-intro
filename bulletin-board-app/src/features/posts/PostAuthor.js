90853c9f71d35b57b530713060723e7de249b0fd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 12:48:21 +0300

    Added the userId as a parameter to the PostAuthor function

--- bulletin-board-app/src/features/posts/PostAuthor.js
+++ bulletin-board-app/src/features/posts/PostAuthor.js
@@ -3,7 +3,7 @@ import { selectAllUsers } from "../users/usersSlice";
 import React from 'react'
 
 
-const PostAuthor = () => {
+const PostAuthor = ({userId}) => {
     const users=useSelector(selectAllUsers);
 
     const author=users.find(user => user.id === userId);
