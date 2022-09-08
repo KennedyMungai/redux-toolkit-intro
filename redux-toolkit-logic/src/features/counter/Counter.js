41a14fb10ea32ad748f19dcab197f7bf6345083b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 10:00:20 +0300

    Added imports for the useSelector and the useDispatch hooks

---
+++ redux-toolkit-logic/src/features/counter/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+
+
+const Counter = () => {
+  return (
+    <div>Counter</div>
+  )
+}
+
+export default Counter
