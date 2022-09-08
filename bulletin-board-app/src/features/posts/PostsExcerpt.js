bfacb8a9b555c0c844f2323e68dd3b9130b113cc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 15:48:57 +0300

    Moved the article to this file

--- bulletin-board-app/src/features/posts/PostsExcerpt.js
+++ bulletin-board-app/src/features/posts/PostsExcerpt.js
@@ -6,7 +6,15 @@ import ReactionButtons from "./ReactionButtons";
 
 const PostsExcerpt = ({post}) => {
   return (
-    <div>PostsExcerpt</div>
+        <article key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content.substring(0, 100)}</p>
+            <p className="postCredit">
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
+            <ReactionButtons post={post}/>
+        </article>
   )
 }
 
