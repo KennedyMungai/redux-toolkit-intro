caa302556dc5467e550ae0da138c17e877121ebb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 10:27:40 +0300

    Added an import statement for the useState hook

--- redux-toolkit-logic/src/features/counter/Counter.js
+++ redux-toolkit-logic/src/features/counter/Counter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { 
         decrement, 
@@ -6,6 +6,7 @@ import {
         reset,
         incrementByAmount 
     } from './counterSlice';
+
 
 
 const Counter = () => {
