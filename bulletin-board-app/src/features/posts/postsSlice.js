1d6a3b05d5c2a4d46d3fe373ea00f31b1135ec81
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 11:08:33 +0300

    Renamed the file

---
+++ bulletin-board-app/src/features/posts/postsSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = [
+    {id:'1', title: 'Learning Redux Toolkit', content: 'I have heard some good things about this course'},
+    {id:'2', title: 'Slices ....', content: 'I still have no idea what this imported library does, but I have time'},
+]
+
+const postsSlice
