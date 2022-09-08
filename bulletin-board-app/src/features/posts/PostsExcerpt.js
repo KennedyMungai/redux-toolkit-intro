6f5be66c89bd1dd1d8bce4c40386a8d71ca24ad4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 15:46:57 +0300

    Moved the import statements here

---
+++ bulletin-board-app/src/features/posts/PostsExcerpt.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
+
+
+const PostsExcerpt = ({post}) => {
+  return (
+    <div>PostsExcerpt</div>
+  )
+}
+
+export default PostsExcerpt;
