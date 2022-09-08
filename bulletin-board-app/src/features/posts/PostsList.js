b85bf392ed8f32306167ede8e65852056b58b3d4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 15:55:20 +0300

    Added new updated code

--- bulletin-board-app/src/features/posts/PostsList.js
+++ bulletin-board-app/src/features/posts/PostsList.js
@@ -17,7 +17,16 @@ const PostsList = () => {
       }
     }, [postsStatus, dispatch])
     
-    
+    let content;
+
+    if (postsStatus === 'loading') {
+        content=<p>"Loading..."</p>;
+    } else if (postsStatus === 'succeeded') {
+        const orderedPosts=posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+        content=orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />);
+    } else if (postsStatus === 'failed') {
+        content=<p>{error}</p>
+    }
 
     return (
         <section>
