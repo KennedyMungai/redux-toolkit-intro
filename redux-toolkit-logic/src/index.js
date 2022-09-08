4f68415b0e24ee8fc31aab1f5f7eefebb376ced5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 09:47:08 +0300

    Refactored the code and locked the App inside the provider

--- redux-toolkit-logic/src/index.js
+++ redux-toolkit-logic/src/index.js
@@ -9,7 +9,8 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>
+  </React.StrictMode>
 );
