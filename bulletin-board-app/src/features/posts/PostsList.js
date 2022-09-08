7e96258aaef4c44ff542299946207b8ffb4c83c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 15:38:12 +0300

    Added import statements from the slice function

--- bulletin-board-app/src/features/posts/PostsList.js
+++ bulletin-board-app/src/features/posts/PostsList.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect } from 'react';
-import { selectAllPosts } from "./postsSlice";
+import { selectAllPosts, getPostsError, getPostsStatus } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
