6741720d1ad39bd5bc6aee4fa0f989981d67c164
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 13:01:22 +0300

    Added the date value to the initialState

--- bulletin-board-app/src/features/posts/postsSlice.js
+++ bulletin-board-app/src/features/posts/postsSlice.js
@@ -3,8 +3,18 @@ import { sub } from 'date-fns';
 
 
 const initialState = [
-    {id:'1', title: 'Learning Redux Toolkit', content: 'I have heard some good things about this course'},
-    {id:'2', title: 'Slices ....', content: 'I still have no idea what this imported library does, but I have time'},
+    {
+        id:'1', 
+        title: 'Learning Redux Toolkit', 
+        content: 'I have heard some good things about this course',
+        date: sub(new Date(), {minutes: 10}).toISOString
+    },
+    {
+        id:'2', 
+        title: 'Slices ....', 
+        content: 'I still have no idea what this imported library does, but I have time',
+        date: sub(new Date(), {minutes: 10}).toISOString
+    },
 ]
 
 const postsSlice = createSlice({
