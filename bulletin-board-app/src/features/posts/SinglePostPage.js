e6982b080cf6c5f8b38104c62e5c69e3d041734b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-09 11:15:32 +0300

    Added logic to the Single Post Page

---
+++ bulletin-board-app/src/features/posts/SinglePostPage.js
@@ -0,0 +1,34 @@
+import { useSelector } from "react-redux";
+import PostAuthor from "./PostAuthor";
+import { selectPostById } from "./postsSlice";
+import ReactionButtons from "./ReactionButtons";
+import TimeAgo from "./TimeAgo";
+
+
+const SinglePostPage = () => {
+    //Retrive the postId
+
+    const post = useSelector((state) => selectPostById(state, postId));
+
+    if (!post) {
+        return (
+            <section>
+                <h2>Post Not Found!</h2>
+            </section>
+        )
+    }
+
+    return (
+        <article>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <p className="postCredit">
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
+            <ReactionButtons post={post} />
+        </article>
+    )
+};
+
+export default SinglePostPage;
