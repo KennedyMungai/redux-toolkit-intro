12cc8778ebcd0a92f5dfab21208474960b8c78a5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-09 15:33:50 +0300

    Initialized the useDispatch hook to dispatch

--- bulletin-board-app/src/components/Header.js
+++ bulletin-board-app/src/components/Header.js
@@ -4,18 +4,20 @@ import { getCount, increaseCount } from "../features/posts/postsSlice";
 
 
 const Header = () => {
-  return (
-        <header className="Header">
-            <h1>Redux Blog</h1>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="post">Post</Link></li>
-                    <li><Link to="user">User</Link></li>
-                </ul>
-            </nav>
-        </header>
-  )
+    const dispatch = useDispatch();
+
+    return (
+            <header className="Header">
+                <h1>Redux Blog</h1>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="post">Post</Link></li>
+                        <li><Link to="user">User</Link></li>
+                    </ul>
+                </nav>
+            </header>
+    )
 }
 
 export default Header;
