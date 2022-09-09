e3f014b83a7b3ca88bdf51d65c8bb617cc7d2876
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-09 10:36:39 +0300

    Loaded up the users on app startup

--- bulletin-board-app/src/index.js
+++ bulletin-board-app/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import { fetchUsers } from './features/users/usersSlice';
 
 
+store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
