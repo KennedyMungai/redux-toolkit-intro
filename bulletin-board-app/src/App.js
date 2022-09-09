719dc5989959940cb081f2a62dae007403349395
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-09 11:29:14 +0300

    Added imports for the Layout, SinglePostPage and the Route and Routes

--- bulletin-board-app/src/App.js
+++ bulletin-board-app/src/App.js
@@ -1,6 +1,10 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
 import AddPostForm from './features/posts/AddPostForm';
 import PostsList from './features/posts/PostsList';
+import SinglePostPage from './features/posts/SinglePostPage';
+
 
 
 function App() {
