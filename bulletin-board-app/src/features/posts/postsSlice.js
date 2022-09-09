1b2789b93ed6f73f099ee3bb1ca251e9ee7dce42
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-09 13:06:45 +0300

    Added the export function deletePost

--- bulletin-board-app/src/features/posts/postsSlice.js
+++ bulletin-board-app/src/features/posts/postsSlice.js
@@ -39,6 +39,19 @@ export const updatePost = createAsyncThunk('posts/updatePost', async (initialPos
     }
 });
 
+export const deletePost = createAsyncThunk('posts/deletePost', async (initialPost) => {
+    const { id } = initialPost;
+    try {
+        const response = await axios.delete(`${POSTS_URL}/${id}`);
+        if (response?.status === 200) {
+            return initialPost;
+        }
+        return `${response?.status}: ${response?.statusText}`;
+    } catch (err) {
+        return err.message;
+    }
+});
+
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
