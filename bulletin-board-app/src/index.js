36ea47d405ef2fb3459f959c5aaa240fa0d8e2ee
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-09 11:21:52 +0300

    Imported BrowserRouter, Routes and Route from teh react-router-dom

--- bulletin-board-app/src/index.js
+++ bulletin-board-app/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { fetchUsers } from './features/users/usersSlice';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 store.dispatch(fetchUsers());
