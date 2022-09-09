b24b7c66372158d3a59060431c03b0002c1a9452
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-09 11:39:44 +0300

    Limited the substring output and gave it the className of excerpt

--- bulletin-board-app/src/features/posts/PostsExcerpt.js
+++ bulletin-board-app/src/features/posts/PostsExcerpt.js
@@ -9,7 +9,7 @@ const PostsExcerpt = ({post}) => {
   return (
         <article>
             <h3>{post.title}</h3>
-            <p>{post.body.substring(0, 100)}</p>
+            <p className='excerpt'>{post.body.substring(0, 75)}...</p>
             <p className="postCredit">
                 <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date} />
