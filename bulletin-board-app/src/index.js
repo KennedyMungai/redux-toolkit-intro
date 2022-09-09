719b48a138c7903ea00ed3334138a52bc8c778bc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-09 14:40:57 +0300

    Added the import statement for fetchposts to the file

--- bulletin-board-app/src/index.js
+++ bulletin-board-app/src/index.js
@@ -6,6 +6,8 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { fetchUsers } from './features/users/usersSlice';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { fetchPosts } from './features/posts/postsSlice';
+
 
 
 store.dispatch(fetchUsers());
