ef238b37cfba487884853ac996d3e2ed52b77676
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-09 12:04:50 +0300

    Removed the react import

--- bulletin-board-app/src/features/posts/AddPostForm.js
+++ bulletin-board-app/src/features/posts/AddPostForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectAllUsers } from '../users/usersSlice';
 import { addNewPost } from './postsSlice';
