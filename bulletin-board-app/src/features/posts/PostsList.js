5dbabd93193c9836be483a94a335247bc5f81235
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-14 09:07:44 +0300

    Replaced the import for selectAllPosts with the import for selectPosIds

--- bulletin-board-app/src/features/posts/PostsList.js
+++ bulletin-board-app/src/features/posts/PostsList.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { selectAllPosts, getPostsError, getPostsStatus } from "./postsSlice";
+import { selectPostIds, getPostsError, getPostsStatus } from "./postsSlice";
 import PostsExcerpt from "./PostsExcerpt";
 
 
