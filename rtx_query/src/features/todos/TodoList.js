f9f49a6a9cbc06148e34c094c3f52369dbd953e5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-14 11:15:05 +0300

    Used a destructred object to initiate the useGetTodosQuery

--- rtx_query/src/features/todos/TodoList.js
+++ rtx_query/src/features/todos/TodoList.js
@@ -7,7 +7,15 @@ import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react"
 
 const TodoList = () => {
-    const [newTodo, setNewTodo] = useState('')
+    const [newTodo, setNewTodo] = useState('');
+
+    const {
+        data: todos,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetTodosQuery();
 
     const handleSubmit = (e) => {
         e.preventDefault();
