0f9adae2d77c80cf0f91e1397a9675a6062b9793
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-14 11:09:53 +0300

    Added the ApiProvider and the apiSlice imports

--- rtx_query/src/index.js
+++ rtx_query/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSlice } from './features/api/apiSlice';
+
+
 
 ReactDOM.createRoot(document.getElementById('root'))
   .render(
