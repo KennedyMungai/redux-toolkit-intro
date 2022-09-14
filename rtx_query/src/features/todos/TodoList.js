73bc4eabd035e17d0c83ddbed2c21811c4d31ee5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-14 11:37:58 +0300

    Replaced the logic inside the isSuccess state

--- rtx_query/src/features/todos/TodoList.js
+++ rtx_query/src/features/todos/TodoList.js
@@ -56,7 +56,24 @@ const TodoList = () => {
     }
     else if(isSuccess)
     {
-        content=JSON.stringify(todos);
+        content = todos.map(todo => {
+            return (
+                <article key={todo.id}>
+                    <div className='todo'>
+                        <input 
+                            type='checkbox'
+                            checked={todo.completed}
+                            id={todo.id}
+                            onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
+                        />
+                        <label htmlFor={todo.id}>{todo.title}</label>
+                    </div>
+                    <button className='trash' onClick={() => deleteTodo({ id: todo.id })}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                </article>
+            )
+        })
     }
     else if(isError)
     {
