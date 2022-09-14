ff54ea41fa9f09ddf5809e3e10c5520ccd9cdda7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-14 11:22:30 +0300

    Added the addTodo method

--- rtx_query/src/features/api/apiSlice.js
+++ rtx_query/src/features/api/apiSlice.js
@@ -7,6 +7,13 @@ export const apiSlice = createApi({
     endpoints: (builder) => ({
         getTodos: builder.query({
             query: () => '/todos',
+        }),
+        addTodo: builder.mutation({
+            query: (todo) => ({
+                url: '/todos',
+                method: 'POST',
+                body: todo
+            })
         })
     })
 });
